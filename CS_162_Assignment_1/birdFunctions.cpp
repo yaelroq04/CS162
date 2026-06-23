23ed095056f719fe2047577e74fdec6202a813bf
Yael Roque <yael.roque04@outlook.com>
2026-06-22 18:10:47 -0700

    Added file structure

---
+++ CS_162_Assignment_1/birdFunctions.cpp
@@ -0,0 +1,1 @@
+#include "birdFunctions.h"
--- CS_162_Assignment_1/main.cpp
+++ CS_162_Assignment_1/main.cpp
@@ -1,6 +1,12 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+/******************************************************************************
+# Author:           Yael Roque
+# Assignment:       Assignment 1, Structs Review
+# Date:             22/06/2026
+# Description:      Example: This program prompts a user for their name and
+#                   prints a message to the screen.
+# Input:            (What the program asks for, and data type, e.g., string)
+# Output:           (Summary of messages displayed by the program)
+# Sources:          Lab 1 specifications
+#                   and any other substantial aids, like web pages or students
+#                   who helped you.
+#******************************************************************************/
