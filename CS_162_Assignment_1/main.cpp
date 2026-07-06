a7cd650af8aa5d20fd61672ed7f2ae034fb439e7
Yael Roque <yael.roque04@outlook.com>
2026-07-06 10:15:06 -0700

    Created main menu loop

--- CS_162_Assignment_1/main.cpp
+++ CS_162_Assignment_1/main.cpp
@@ -19,6 +19,64 @@ using namespace std;
 int main() {
     Bird birds[ARR_SIZE];
     int count = 0;
-    char filename[STR_SIZE];
+    char filename[STR_SIZE] = "";
+    char outFilename[STR_SIZE] = "";
     char choice = ' ';
+    ifstream testFile;
+
+    cout << "Time for some bird spotting!" << endl << endl;
+
+    cout << "What is the file name to open? ('exit' if you don't "
+         << "have one): ";
+    cin.getline(filename, STR_SIZE);
+    testFile.open(filename);
+
+    while (!testFile.is_open() && strcmp(filename, "exit") != 0 &&
+           strcmp(filename, "quit") != 0) {
+        cout << "File " << filename << " did not open. Please try "
+             << "again or type 'exit': ";
+        cin.getline(filename, STR_SIZE);
+        testFile.open(filename);
+           }
+
+    if (testFile.is_open()) {
+        testFile.close();
+        count = loadBirds(birds, filename);
+        cout << endl << count << " birds were successfully loaded."
+             << endl;
+        printBirds(birds, count);
+
+        do {
+            cout << endl << "What would you like to do? (P)rint the "
+                 << "list, (A)dd, or (Q)uit? ";
+            cin >> choice;
+            cin.ignore(1000, '\n');
+            choice = toupper(choice);
+
+            if (choice == 'P') {
+                printBirds(birds, count);
+            }
+            else if (choice == 'A') {
+                addBird(birds, count);
+            }
+            else if (choice == 'Q') {
+                cout << endl << "What file name would you like to use "
+                     << "for the output file: ";
+                cin.getline(outFilename, STR_SIZE);
+                writeBirds(birds, count, outFilename);
+            }
+            else {
+                cout << endl << choice << " is not a valid option. "
+                     << "Please try again." << endl;
+            }
+        } while (choice != 'Q');
+    }
+    else {
+        cout << endl << "Exit was entered, so no birds were loaded."
+             << endl;
+    }
+
+    cout << endl << "Keep those cameras ready! Until next time." << endl;
+
+    return 0;
 }
